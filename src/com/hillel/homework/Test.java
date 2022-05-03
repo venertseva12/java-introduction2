7681addccbc1cb73e23483a44dffd9ca2b8afe71
alina <alinka.ustishka@gmail.com>
2022-05-04 00:20:39 +0300

    add homework 1

--- src/com/hillel/homework/Test.java
+++ src/com/hillel/homework/Test.java
@@ -1,4 +1,12 @@
 package com.hillel.homework;
 
-public class Test {
+public class Test
+{
+    public static void main(String[] args) {
+        long myLong = 4843632;
+        System.out.println(myLong);
+
+        int myInt = 2636101;
+    System.out.println(myInt);
+    }
 }
