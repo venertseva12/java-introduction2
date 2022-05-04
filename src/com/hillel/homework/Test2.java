20ac6426065aa6fa5c317391df54df8c0aa7a4dc
alina <alinka.ustishka@gmail.com>
2022-05-05 00:43:25 +0300

    add homework 1

--- src/com/hillel/homework/Test2.java
+++ src/com/hillel/homework/Test2.java
@@ -4,12 +4,19 @@ public class Test2 {
     public static void main(String[] args) {
 
         int a = 2342;
-        System.out.println("a=" +a);
+        System.out.println("a=" + a);
         int b = -875;
-        System.out.println("b="+b);
+        System.out.println("b=" + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+
 
     }
-
 
 
 }
