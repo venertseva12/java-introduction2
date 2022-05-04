df44b46d368163a6ad0f81e72f950dc53090bef7
alina <alinka.ustishka@gmail.com>
2022-05-05 00:20:09 +0300

    add homework 1

--- src/com/hillel/homework/Test2.java
+++ src/com/hillel/homework/Test2.java
@@ -1,4 +1,16 @@
 package com.hillel.homework;
 
 public class Test2 {
+    public static void main(String[] args) {
+
+        int a = 2342;
+        System.out.println("a=" +a);
+        int b = -875;
+        System.out.println("b="+b);
+
+    }
+
+
+
 }
+
